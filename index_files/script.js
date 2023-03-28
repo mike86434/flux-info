1f20756d9d608aaf9ab50b840101675d0c2f1e86
mike8643 <nguyen95465@yahoo.com>
2023-03-28 13:29:35 -0400

    fix PA's released and calcs

--- index_files/script.js
+++ index_files/script.js
@@ -39,7 +39,7 @@ var curRadio = "USD";
 var rankRadioValue = "IP";
 var vpsCost = 0;
 var aprFluxMonth = 0;
-var paValue = 10 / 7;
+var paValue = 10 / 8;
 
 /*********************************************
 Main function - Called at website opening
